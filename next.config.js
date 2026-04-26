1268929cf615fc3d96cc1061ffbb25925a3aacc9
design-indra <ghostersoloist@gmail.com>
2026-04-27 02:00:18 +0700

    fix: simplify next.config with standalone output

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["next-auth"],
-  experimental: {
-    serverComponentsExternalPackages: ["bcryptjs"],
-  },
+  output: "standalone",
 };
 
 module.exports = nextConfig;
